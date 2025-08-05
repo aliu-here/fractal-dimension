2abb460f44a946430660681eb4a1d0701bcc0022
aliu <andrew.yc.liu@gmail.com>
2025-08-05 15:20:43 -0700

    try to use GPU for calculation

--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,14 @@
 import cv2
 import math
+import cupy
 import numpy as np
 import copy
 
 def readfile(path):
     image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
-    image = image.astype(np.uint8)
-    return image
+    image = image.astype(cupy.uint8)
+    return cupy.asarray(image)
 
 def fillalpha(image):
     trans_mask = image[:,:,3] == 0
@@ -19,41 +20,46 @@ def tograyscale(image):
     return image
 
 def detect_edges(image, thresh1=0, thresh2=80):
-    edges = cv2.Canny(image, thresh1, thresh2, 5)
-    return edges
+    edges = cv2.Canny(image.get(), thresh1, thresh2, 5)
+    return cupy.asarray(edges)
+
+def detect_edges_GPU(image, detector):
+    dstImg = detector.detect(image)
+    return dstImg.download()
 
 def detect_edges_sobel(img):
     img = cv2.GaussianBlur(img, (25, 25), 0)
     grad_x = cv2.Sobel(img, cv2.CV_64F, 1, 0)
     grad_y = cv2.Sobel(img, cv2.CV_64F, 0, 1)
-    grad = np.sqrt(grad_x**2 + grad_y**2)
-    grad_norm = (grad * 255 / grad.max()).astype(np.uint8)
+    grad = cupy.sqrt(grad_x**2 + grad_y**2)
+    grad_norm = (grad * 255 / grad.max()).astype(cupy.uint8)
     return grad_norm
 
 def high_pass_filter(img, filter_size):
     rows, cols, depth = img.shape
     optimal_rows, optimal_cols = (cv2.getOptimalDFTSize(rows), cv2.getOptimalDFTSize(cols))
-    tmp = np.zeros((optimal_rows, optimal_cols, depth))
+    tmp = cupy.zeros((optimal_rows, optimal_cols, depth))
     tmp[:rows,:cols] = img[:,:]
     img = tmp
     rows, cols = optimal_rows, optimal_cols
-    f_r, f_g, f_b = np.fft.fft2(np.float64(img[:,:,0]) / 255), np.fft.fft2(np.float64(img[:,:,1]) / 255), np.fft.fft2(np.float64(img[:,:,2]) / 255)
-    fshift_r, fshift_g, fshift_b = np.fft.fftshift(f_r), np.fft.fftshift(f_g), np.fft.fftshift(f_b)  
+    f_r, f_g, f_b = cupy.fft.fft2(cupy.array(img[:,:,0], dtype=np.float32) / 255), cupy.fft.fft2(cupy.array(img[:,:,1], dtype=np.float32) / 255), cupy.fft.fft2(cupy.array(img[:,:,2], dtype=np.float32) / 255)
+    fshift_r, fshift_g, fshift_b = cupy.fft.fftshift(f_r), cupy.fft.fftshift(f_g), cupy.fft.fftshift(f_b)  
 
     center_row = rows // 2
     center_col = cols // 2
     transformed = [copy.deepcopy(fshift_r), copy.deepcopy(fshift_g), copy.deepcopy(fshift_b)]
     for channel in transformed:
-        mask = np.zeros((rows, cols))
+        mask = cupy.zeros((rows, cols))
         mask[center_row - filter_size:center_row + filter_size + 1, center_col - filter_size : center_col + filter_size + 1] = 1
         channel[mask != 0] = 0
 
-    high_pass = np.zeros(img.shape)
+    high_pass = cupy.zeros(img.shape)
     high_pass[:,:,3] = 1
     for i in range(3):
-        f_ishift = np.fft.ifftshift(transformed[i])
-        high_pass[:,:,i] = np.real(np.fft.ifft2(f_ishift))
-    return np.array(np.clip(high_pass * 255, 0, 255), dtype='uint8')
+        f_ishift = cupy.fft.ifftshift(transformed[i])
+        high_pass[:,:,i] = cupy.real(cupy.fft.ifft2(f_ishift))
+    return cupy.array(cupy.clip(high_pass * 255, 0, 255), dtype='uint8')
+
 
 def calc_sum_area(table, br, tl):
     return table[tl[0]][tl[1]] + table[br[0]][br[1]] - table[tl[0]][br[1]] - table[br[0]][tl[1]]
@@ -63,7 +69,7 @@ def boxcount(image, sizes):
     y = []
     rows, cols = image.shape
 
-    prefsum = np.zeros((rows + 1, cols + 1))
+    prefsum = cupy.zeros((rows + 1, cols + 1))
     prefsum[1:,1:] = image.cumsum(axis=0).cumsum(axis=1)
     for box_size in sizes:
         black_box_count = 0
@@ -81,7 +87,7 @@ def boxcount_4x(image, sizes):
     rows, cols = image.shape
 
     prefsum = np.zeros((rows + 1, cols + 1))
-    prefsum[1:,1:] = image.cumsum(axis=0).cumsum(axis=1)
+    prefsum[1:,1:] = (image.cumsum(axis=0).cumsum(axis=1)).get()
 
     for box_size in sizes:
         black_box_count = 0
