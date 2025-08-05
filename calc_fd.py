1dab8ca65e566dcb9e1fa9df9b5ba1254cfa6ead
aliu <andrew.yc.liu@gmail.com>
2025-08-05 15:26:44 -0700

    divide things into groups in a slightly better way

--- calc_fd.py
+++ calc_fd.py
@@ -16,28 +16,42 @@ quality = manager.list()
 
 box_sizes = [2 ** n for n in range(1, 8)]
 
-def calc_fd(path, label, fds, paths, quality):
-    edges = detect_edges(high_pass_filter(fillalpha(readfile(path)), 0))
-    edges = edges[:,:] > 127
-    x, y = boxcount_4x(edges, box_sizes)
-    fd, _ = linregression(x, y)
-    print(fd, path, label)
-    fds.append(fd)
-    paths.append(path)
-    quality.append(label)
+def calc_fd(files, labels, fds, paths, quality):
+    for path, label in zip(files, labels):
+        edges = detect_edges(high_pass_filter(fillalpha(readfile(path)), 0))
+        edges = edges[:,:] > 127
+        x, y = boxcount_4x(edges, box_sizes)
+        fd, _ = linregression(x, y)
+        print(fd, path, label)
+        fds.append(fd)
+        paths.append(path)
+        quality.append(label)
+thread_count = 16
+
+divided_files = []
+divided_labels = []
+for i in range(thread_count):
+    divided_files.append([])
+    divided_labels.append([])
+counter = 0
 
 threads = []
 
 for filename in glob.glob(bad_path + "/*.jpg"):
-    #    calc_fd(filename, "Bad")
-    t = Process(target=calc_fd, args=(filename, "Bad", fds, paths, quality))
-    threads.append(t)
+    divided_files[counter].append(filename)
+    divided_labels[counter].append("Bad")
+    counter += 1
+    counter %= thread_count
 
 for filename in glob.glob(good_path + "/*.jpg"):
-    #calc_fd(filename, "Good")
-    t = Process(target=calc_fd, args=(filename, "Good", fds, paths, quality))
+    divided_files[counter].append(filename)
+    divided_labels[counter].append("Good")
+    counter += 1
+    counter %= thread_count
+
+for file_group, label_group in zip(divided_files, divided_labels):
+    t = Process(target = calc_fd, args = (file_group, label_group, fds, paths, quality))
     threads.append(t)
-    cv2.imwrite("test.png", detect_edges(high_pass_filter(fillalpha(readfile(filename)), 0)))
 
 for t in threads:
     t.start()
